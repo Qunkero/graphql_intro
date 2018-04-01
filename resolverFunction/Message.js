549fd12c9431c129d443767946ac2f03be795e7a
Qunkero <dron.andrew2011@yandex.ua>
2018-04-01 21:45:39 +0300

    fake database, simple mutations

---
+++ resolverFunction/Message.js
@@ -0,0 +1,41 @@
+const fakeDatabase = {};
+
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+const rootMessage = {
+  getMessage: function ({id}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function ({input}) {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function ({id, input}) {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  getAllId: () => {
+    return Object.keys(fakeDatabase)
+  }
+}
+
+module.exports = {
+  Message,
+  rootMessage,
+}
---
+++ resolverFunction/rootResolve.js
@@ -0,0 +1,29 @@
+module.exports = {
+  hello: () => 'Hello world!',
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
+  rollDice: function ({numDice, numSides}) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  },
+  getDie: function ({numSides}) {
+    return new RandomDie(numSides || 6);
+  },
+  setMessage: function ({message}) {
+    fakeDatabase.message = message;
+    return message;
+  },
+  getMessage: function () {
+    return fakeDatabase.message;
+  }
+};
